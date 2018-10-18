61ca3a3152f7bfbdff2f641d11731a617a397222
Алексей Стебле <asteblo@gmail.com>
2018-10-18 10:55:04 +0300

    bat

---
+++ Upload.bat
@@ -0,0 +1,10 @@
+git add *.cpp 
+git add *.sln 
+git add *.vcxproj 
+git add *.vcxproj.filters 
+git add *.h 
+git add *.bat 
+git add *.user 
+git commit -am "%DATE% %TIME%" 
+git push mylabs master 
+pause
